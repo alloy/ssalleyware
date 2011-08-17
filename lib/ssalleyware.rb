002a7f788fe81ad7da7191a6caf30594aaeba0f0
Eloy Duran <eloy.de.enige@gmail.com>
2011-08-17 23:43:56 +0200

    Make specs green.

--- lib/ssalleyware.rb
+++ lib/ssalleyware.rb
@@ -1,9 +1,24 @@
 module SSAlleyWare
+  DEBIAN_UBUNTU_ARCH_LINUX = "/etc/ssl/certs/ca-certificates.crt"
+  FEDORA_RHEL              = "/etc/pki/tls/certs/ca-bundle.crt"
+  OPENSUSE_SLE             = "/etc/ssl/ca-bundle.pem"
+  SYSTEM_CA_CERT_PATHS     = [DEBIAN_UBUNTU_ARCH_LINUX, FEDORA_RHEL, OPENSUSE_SLE]
+
   def self.ca_cert_file=(path)
     @ca_cert_file = path
   end
 
   def self.ca_cert_file
-    @ca_cert_file
+    @ca_cert_file ||= system_ca_cert_file || bundled_ca_cert_file
+  end
+
+  def self.system_ca_cert_file
+    SYSTEM_CA_CERT_PATHS.find do |path|
+      File.exist?(path) && File.readable?(path)
+    end
+  end
+
+  def self.bundled_ca_cert_file
+    File.expand_path('../cacert.pem', __FILE__)
   end
 end
--- spec/ssalleyware_spec.rb
+++ spec/ssalleyware_spec.rb
@@ -9,27 +9,45 @@ $:.unshift File.expand_path("../../lib", __FILE__)
 require "ssalleyware"
 
 describe "SSAlleyWare, concerning the root CA certificates file" do
+  before do
+    SSAlleyWare.ca_cert_file = nil
+    File.stubs(:readable?).returns(true)
+  end
+
+  def stub(stub_path)
+    SSAlleyWare::SYSTEM_CA_CERT_PATHS.each do |path|
+      File.stubs(:exist?).with(path).returns(path == stub_path)
+    end
+  end
+
   it "returns the Debian/Ubuntu/Arch Linux one" do
-    SSAlleyWare.ca_cert_file.should == "/etc/ssl/certs/ca-certificates.crt"
+    stub(SSAlleyWare::DEBIAN_UBUNTU_ARCH_LINUX)
+    SSAlleyWare.ca_cert_file.should == SSAlleyWare::DEBIAN_UBUNTU_ARCH_LINUX
   end
 
   it "returns the Fedora/RHEL one" do
-    SSAlleyWare.ca_cert_file.should == "/etc/pki/tls/certs/ca-bundle.crt"
+    stub(SSAlleyWare::FEDORA_RHEL)
+    SSAlleyWare.ca_cert_file.should == SSAlleyWare::FEDORA_RHEL
   end
 
-  it "returns the Debian/Ubuntu/Arch Linux one" do
-    SSAlleyWare.ca_cert_file.should == "/etc/ssl/ca-bundle.pem"
+  it "returns the openSUSE/SLE" do
+    stub(SSAlleyWare::OPENSUSE_SLE)
+    SSAlleyWare.ca_cert_file.should == SSAlleyWare::OPENSUSE_SLE
   end
 
   it "returns the one bundled with the gem if none of the system versions is found" do
-    SSAlleyWare.ca_cert_file.should == File.expand_path("../../lib/cacert-20110817.pem", __FILE__)
+    File.stubs(:exist?).returns(false)
+    SSAlleyWare.ca_cert_file.should == File.expand_path("../../lib/cacert.pem", __FILE__)
   end
 
   it "returns the one bundled with the gem if none of the system versions is readable" do
-    SSAlleyWare.ca_cert_file.should == File.expand_path("../../lib/cacert-20110817.pem", __FILE__)
+    File.stubs(:exist?).returns(true)
+    File.stubs(:readable?).returns(false)
+    SSAlleyWare.ca_cert_file.should == File.expand_path("../../lib/cacert.pem", __FILE__)
   end
 
   it "always returns the one that was specified by the user" do
+    File.stubs(:exist?).returns(true)
     SSAlleyWare.ca_cert_file = '/path/to/cacert.pem'
     SSAlleyWare.ca_cert_file.should == '/path/to/cacert.pem'
   end
