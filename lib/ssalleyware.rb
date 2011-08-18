eff62110f077e49f364293159f4d5fc286281a4c
Eloy Duran <eloy.de.enige@gmail.com>
2011-08-18 02:18:47 +0200

    Start on the certificate verification method that rescues exceptions.

--- lib/ssalleyware.rb
+++ lib/ssalleyware.rb
@@ -2,7 +2,8 @@ module SSAlleyWare
   DEBIAN_UBUNTU_ARCH_LINUX = "/etc/ssl/certs/ca-certificates.crt"
   FEDORA_RHEL              = "/etc/pki/tls/certs/ca-bundle.crt"
   OPENSUSE_SLE             = "/etc/ssl/ca-bundle.pem"
-  SYSTEM_CA_CERT_PATHS     = [DEBIAN_UBUNTU_ARCH_LINUX, FEDORA_RHEL, OPENSUSE_SLE]
+  SYSTEM_CA_CERT_FILES     = [DEBIAN_UBUNTU_ARCH_LINUX, FEDORA_RHEL, OPENSUSE_SLE]
+  BUNDLED_CA_CERT_FILE     = File.expand_path('../cacert.pem', __FILE__)
 
   def self.ca_cert_file=(path)
     @ca_cert_file = path
@@ -13,12 +14,58 @@ module SSAlleyWare
   end
 
   def self.system_ca_cert_file
-    SYSTEM_CA_CERT_PATHS.find do |path|
+    SYSTEM_CA_CERT_FILES.find do |path|
       File.exist?(path) && File.readable?(path)
     end
   end
 
   def self.bundled_ca_cert_file
-    File.expand_path('../cacert.pem', __FILE__)
+    BUNDLED_CA_CERT_FILE
+  end
+
+  def self.use_bundled_ca_cert_file!
+    self.ca_cert_file = bundled_ca_cert_file
+  end
+
+  module CertificateVerification
+    def self.included(_)
+      require 'openssl'
+      super
+    end
+
+    def self.extended(_)
+      require 'openssl'
+      super
+    end
+
+    def ca_store
+      unless @ca_store
+        if file = SSAlleyWare.ca_cert_file
+          @ca_store = OpenSSL::X509::Store.new
+          @ca_store.add_file(file)
+        else
+          fail "you must specify a file with root CA certificates through `SSAlleyWare.ca_cert_file='"
+        end
+      end
+      @ca_store
+    end
+
+    def ssl_verify_peer(cert_string)
+      @last_seen_cert = OpenSSL::X509::Certificate.new(cert_string)
+      if ca_store.verify(@last_seen_cert)
+        begin
+          ca_store.add_cert(@last_seen_cert)
+        rescue OpenSSL::X509::StoreError => e
+          # This is so lame, but there appears to be no way to check this in a simple way.
+          #
+          # TODO It _might_ be possible to do this with the C api.
+          raise e unless e.message == 'cert already in hash table'
+        end
+        true
+      else
+        fail OpenSSL::OpenSSLError.new("unable to verify the server certificate of `#{@hostname}'")
+        false
+      end
+    end
   end
 end
--- spec/ssalleyware_spec.rb
+++ spec/ssalleyware_spec.rb
@@ -15,7 +15,7 @@ describe "SSAlleyWare, concerning the root CA certificates file" do
   end
 
   def stub(stub_path)
-    SSAlleyWare::SYSTEM_CA_CERT_PATHS.each do |path|
+    SSAlleyWare::SYSTEM_CA_CERT_FILES.each do |path|
       File.stubs(:exist?).with(path).returns(path == stub_path)
     end
   end
@@ -37,18 +37,73 @@ describe "SSAlleyWare, concerning the root CA certificates file" do
 
   it "returns the one bundled with the gem if none of the system versions is found" do
     File.stubs(:exist?).returns(false)
-    SSAlleyWare.ca_cert_file.should == File.expand_path("../../lib/cacert.pem", __FILE__)
+    SSAlleyWare.ca_cert_file.should == SSAlleyWare::BUNDLED_CA_CERT_FILE
   end
 
   it "returns the one bundled with the gem if none of the system versions is readable" do
     File.stubs(:exist?).returns(true)
     File.stubs(:readable?).returns(false)
-    SSAlleyWare.ca_cert_file.should == File.expand_path("../../lib/cacert.pem", __FILE__)
+    SSAlleyWare.ca_cert_file.should == SSAlleyWare::BUNDLED_CA_CERT_FILE
   end
 
   it "always returns the one that was specified by the user" do
     File.stubs(:exist?).returns(true)
     SSAlleyWare.ca_cert_file = '/path/to/cacert.pem'
     SSAlleyWare.ca_cert_file.should == '/path/to/cacert.pem'
+  end
+
+  it "always uses the one bundled with the gem" do
+    File.stubs(:exist?).returns(true)
+    SSAlleyWare.use_bundled_ca_cert_file!
+    SSAlleyWare.ca_cert_file.should == SSAlleyWare::BUNDLED_CA_CERT_FILE
+  end
+end
+
+class Verifyer
+  include SSAlleyWare::CertificateVerification
+end
+
+describe "SSAlleyWare::CertificateVerification, when verifying certificates" do
+  def fixture_cert(name)
+    File.expand_path("../fixtures/certificates/#{name}", __FILE__)
+  end
+
+  def fixture_cert_read(name)
+    File.read(fixture_cert(name))
+  end
+
+  before do
+    @verifyer                   = Verifyer.new
+    SSAlleyWare.ca_cert_file    = fixture_cert('root_ca.pem')
+    @root_ca_cert               = fixture_cert_read('root_ca.pem')
+    @intermediate_ca_cert       = fixture_cert_read('intermediate_ca.pem')
+    @server_cert                = fixture_cert_read('server.pem')
+    @other_intermediate_ca_cert = fixture_cert_read('other_intermediate_ca.pem')
+  end
+
+  it "returns true for each successive certificate if signed by the ancestors in the chain" do
+    @verifyer.ssl_verify_peer(@intermediate_ca_cert).should == true
+    @verifyer.ssl_verify_peer(@server_cert).should == true
+  end
+
+  it "returns false if a certificate is not signed by the ancestor chain" do
+    @verifyer.ssl_verify_peer(@other_intermediate_ca_cert).should == true
+    @verifyer.expects(:fail).with { |error| error.class == OpenSSL::OpenSSLError }
+    @verifyer.ssl_verify_peer(@server_cert).should == false
+  end
+
+  it "does not fail if a root CA certificate is given, which the server *may* do" do
+    lambda {
+      @verifyer.ssl_verify_peer(@root_ca_cert).should == true
+    }.should.not.raise
+    @verifyer.ssl_verify_peer(@intermediate_ca_cert).should == true
+    @verifyer.ssl_verify_peer(@server_cert).should == true
+  end
+
+  it "does not fail if any other certificate is send multiple times, which some servers do (imap.gmail.com)" do
+    @verifyer.ssl_verify_peer(@intermediate_ca_cert).should == true
+    @verifyer.ssl_verify_peer(@intermediate_ca_cert).should == true
+    @verifyer.ssl_verify_peer(@intermediate_ca_cert).should == true
+    @verifyer.ssl_verify_peer(@server_cert).should == true
   end
 end
